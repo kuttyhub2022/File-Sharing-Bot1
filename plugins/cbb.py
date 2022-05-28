16f952007948816d101c69fc5437ca372580c31f
aakash2021-cap <84575647+aakash2021-cap@users.noreply.github.com>
2022-05-28 16:58:57 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a>@FLIGHERThis Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/aakash2021-cap/File-Sharing-Bot1'>Click here</a>\n○ Channel : @Is_Movie_Time</b>",
+            text = f"<b>○ Creator : <a>@FLIGHER</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/aakash2021-cap/File-Sharing-Bot1'>Click here</a>\n○ Channel : @Is_Movie_Time</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
