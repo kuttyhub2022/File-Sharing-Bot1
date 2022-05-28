4cea2fb9a411333c7cae9dc7d933a5450553cce8
aakash2021-cap <84575647+aakash2021-cap@users.noreply.github.com>
2022-05-28 16:33:09 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -49,7 +49,7 @@ class Bot(Client):
             sys.exit()
 
         self.set_parse_mode("html")
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by 𝘾𝙤𝙙𝙚 𝕏 𝘽𝙤𝙩𝙯\nhttps://t.me/CodeXBotz")
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by ⲘⲨⲊⲦⲈRⲒⲞ \nhttps://t.me/FLIGHER")
         self.username = usr_bot_me.username
 
     async def stop(self, *args):
